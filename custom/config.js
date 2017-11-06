748618a90d18ad050204056b79667854d459b79f
tmartinez <tmartinez@switchfly.com>
2017-11-06 15:18:37 -0700

    rework, refactoring and customizable images ;)

---
+++ custom/config.js
@@ -0,0 +1,29 @@
+(function(box) {
+
+    box.BJ = box.BJ || {};
+
+    /*
+     * Animations available
+     *
+     * - shake
+     * - shake-little
+     * - shake-slow
+     * - shake-hard
+     * - shake-horizontal
+     * - shake-vertical
+     * - shake-rotate
+     * - shake-opacity
+     * - shake-crazy
+     * - shake-chunk
+     */
+
+    box.BJ.custom = {
+        "Pozolito": {
+            "nick": "PozolitoMx",
+            "image_top": "img/t_top.png",
+            "image_bottom": "custom/p_bot.png",
+            "shake_style": "shake-slow"
+        }
+    };
+
+}(window || this));
--- js/talking_heads.js
+++ js/talking_heads.js
@@ -14,25 +14,33 @@ var client = new Discord.Client({
 });
 
 var updateTalkingFaces = function () {
-    var shakeClasses = 'shake-chunk shake-constant'; 
     var $talkingHead = $head.clone().show();
 
     $('.users').empty();
-    
+
     for (id in users) {
         if (id !== client.id) {
             $talkingHead = $head.clone().show();
+
+            var customCharacter = window.BJ.custom[users[id].username] || {};
+            var topImg = customCharacter.image_top || 'img/t_top.png';
+            var topImgSleeping = customCharacter.image_bottom || 'img/t_top_sleeping.png';
+            var bottomImg = customCharacter.image_bottom || 'img/t_bot.png';
+            var shakeStyle = 'shake-constant ' + (customCharacter.shake_style || 'shake-chunk');
+            var nickname = customCharacter.nick || users[id].username;
+
             $('.users').append($talkingHead);
-            //$talkingHead.find()
-            $talkingHead.find('.name').append(users[id].username);
+            $talkingHead.find('.top img').attr('src', topImg);
+            $talkingHead.find('.bot img').attr('src', bottomImg);
+            $talkingHead.find('.name').append(nickname);
 
             if (users[id].speaking) {
-                $talkingHead.find('.top img').attr('src', 'img/t_top.png');
-                $talkingHead.find('.top').addClass(shakeClasses);
+                $talkingHead.find('.top img').attr('src', topImg);
+                $talkingHead.find('.top').addClass(shakeStyle);
             } else {
-                $talkingHead.find('.top').removeClass(shakeClasses);
+                $talkingHead.find('.top').removeClass(shakeStyle);
                 if (!users[id].time_since_last_speak || time - users[id].time_since_last_speak >= config.sleepAfterSeconds) {
-                    $talkingHead.find('.top img').attr('src', 'img/t_top_sleeping.png');
+                    $talkingHead.find('.top img').attr('src', topImgSleeping);
                 } 
             }
         }
@@ -56,7 +64,7 @@ $(document).ready(function () {
                 var speaking = client.users[userID].username;
                 users = getConnectedUsers();
 
-                for(id in users) { 
+                for(id in users) {
                     if (users[id].user_id === userID) {
                         // adding a custom property 
                         users[id].speaking = isSpeaking;
